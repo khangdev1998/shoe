cd61b5690eeab92e7b1631f674b206d03fd1b86e
khangdev1998 <khangdev171998@gmail.com>
2021-08-09 11:02:55 +0700

    first upload website

---
+++ script.js
@@ -0,0 +1,14 @@
+$('.owl-carousel').owlCarousel({
+    autoplay: true,
+    autoplayTimeout: 4000,
+    loop: true,
+    margin: 0,
+    nav: true,
+    navText: ['<ion-icon name="chevron-back-outline"></ion-icon>',
+    '<ion-icon name="chevron-forward-outline"></ion-icon>'],
+    dots: false,
+    items: 1,
+    autoplayspeed: 500,
+    smartSpeed: 500,
+    autoplayHoverPause: true,
+})
